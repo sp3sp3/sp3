52c931704f9afb777120a629bc2068274b1e3ff7
sp3sp3 <>
2023-11-22 23:21:14 +0100

    add more prisma stuff

---
+++ prisma/migrations/20231122214019_add_project_name/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - Added the required column `name` to the `Project` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+INSERT INTO "new_Project" ("id") SELECT "id" FROM "Project";
+DROP TABLE "Project";
+ALTER TABLE "new_Project" RENAME TO "Project";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
