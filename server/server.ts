c8c22fb60a465f1f51f24aa197074050b32e6bce
sp3sp3 <>
2023-11-23 22:14:06 +0100

    Add initial route

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,26 @@
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
-
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", async function handler(request, reply) {
-  return { hellow: "world" };
+const prisma = new PrismaClient();
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
 });
 
-try {
-  await fastify.listen({ port: 3000 });
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
-}
+fastify.get("/projects", async function (request, reply) {
+  const projects = await prisma.project.findMany({});
+  reply.send({ projects: projects });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
