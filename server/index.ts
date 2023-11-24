8bfc4aa3f97626e2b4dbfad9f109b9fc9d7b798e
sp3sp3 <>
2023-11-24 17:58:41 +0100

    Refactor routes to their own files

--- server/index.ts
+++ server/index.ts
@@ -1,42 +1,11 @@
-import { PrismaClient, Project } from "@prisma/client";
 import express from "express";
-import { Send } from "express-serve-static-core";
+import { projectRoutes } from "./routes/projects";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const prisma = new PrismaClient();
-
-export interface TypedRequestBody<T> extends Express.Request {
-  body: T;
-}
-
-export interface TypedResponse<ResBody> extends Express.Response {
-  json: Send<ResBody, this>;
-}
-
-app.get("/projects", async (_, res: TypedResponse<{ projects: Project[] }>) => {
-  const projects = await prisma.project.findMany({});
-  res.json({ projects: projects });
-});
-
-app.post(
-  "/projects",
-  async (
-    req: TypedRequestBody<{ name: string }>,
-    res: TypedResponse<{ project: Project }>,
-  ) => {
-    const { name } = req.body;
-
-    const project = await prisma.project.create({
-      data: {
-        name: name,
-      },
-    });
-    console.log(res);
-    res.json({ project: project });
-  },
-);
+app.use(express.json());
+app.use("/projects", projectRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
