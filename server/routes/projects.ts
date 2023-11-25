936e56f47317f4fde70d88f9d44a4ec81fe3aa97
sp3sp3 <>
2023-11-25 11:24:22 +0100

    Refactor routes with handlers to prepare for tests

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -14,48 +14,44 @@ export interface TypedResponse<T> extends Response {
   json: Send<T, this>;
 }
 
-projectRoutes.get(
-  "/",
-  async (
-    _: TypedRequestBody<{}>,
-    res: TypedResponse<{ projects: Project[] }>,
-  ) => {
-    const projects = await prisma.project.findMany({});
-    res.json({ projects: projects });
-  },
-);
+const getProjectsHandler = async (
+  _: TypedRequestBody<{}>,
+  res: TypedResponse<{ projects: Project[] }>,
+) => {
+  const projects = await prisma.project.findMany({});
+  res.json({ projects: projects });
+};
 
-projectRoutes.get(
-  "/:id",
-  async (
-    req: TypedRequestBody<{ id: string }>,
-    res: TypedResponse<{ project: Project }>,
-  ) => {
-    const project = await prisma.project.findUnique({
-      where: { id: Number(req.body.id) },
-    });
+const getProjectByIdHandler = async (
+  req: TypedRequestBody<{ id: string }>,
+  res: TypedResponse<{ project: Project }>,
+) => {
+  const project = await prisma.project.findUnique({
+    where: { id: Number(req.body.id) },
+  });
 
-    if (!project) {
-      res.status(404).send(`Project id=${req.body.id} not found`);
-    } else {
-      res.json({ project: project });
-    }
-  },
-);
-
-projectRoutes.post(
-  "/",
-  async (
-    req: TypedRequestBody<{ name: string }>,
-    res: TypedResponse<{ project: Project }>,
-  ) => {
-    const { name } = req.body;
-
-    const project = await prisma.project.create({
-      data: {
-        name: name,
-      },
-    });
+  if (!project) {
+    res.status(404).send(`Project id=${req.body.id} not found`);
+  } else {
     res.json({ project: project });
-  },
-);
+  }
+};
+
+const createProjectHandler = async (
+  req: TypedRequestBody<{ name: string; parentId?: string }>,
+  res: TypedResponse<{ project: Project }>,
+) => {
+  const { name, parentId } = req.body;
+
+  const project = await prisma.project.create({
+    data: {
+      name: name,
+      parentId: parentId ? Number(parentId) : null,
+    },
+  });
+  res.json({ project: project });
+};
+
+projectRoutes.get("/", getProjectsHandler);
+projectRoutes.get("/:id", getProjectByIdHandler);
+projectRoutes.post("/", createProjectHandler);
