6566d9290b43297feea8c81af92f2043716e9c75
sp3sp3 <>
2023-11-25 21:14:08 +0100

    Refactor to move types outside of projects routes

---
+++ server/types.d.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express-serve-static-core";
+import { Send } from "express-serve-static-core";
+
+export interface TypedRequestBody<T> extends Request {
+  body: T;
+}
+
+export interface TypedResponse<T> extends Response {
+  json: Send<T, this>;
+}
