6be15017e25aa9e36d00b0cc1b8939cc31111102
sp3sp3 <>
2023-11-25 21:14:08 +0100

    Refactor to move types outside of projects routes

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -1,18 +1,10 @@
 import { PrismaClient, Project } from "@prisma/client";
-import { Send } from "express-serve-static-core";
-import { Router, Response, Request } from "express";
+import { Router } from "express";
+import { TypedRequestBody, TypedResponse } from "../types";
 
 export const projectRoutes = Router();
 
 const prisma = new PrismaClient();
-
-export interface TypedRequestBody<T> extends Request {
-  body: T;
-}
-
-export interface TypedResponse<T> extends Response {
-  json: Send<T, this>;
-}
 
 export const getProjectsHandler = async (
   _: TypedRequestBody<{}>,
