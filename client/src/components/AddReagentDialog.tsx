317a3ee930ecc6a364ac960d9667c6fa90b0b38b
sp3sp3 <>
2023-12-18 15:27:04 +0100

    wip: switch between smiles or name when adding a reagent
    will search pubchem when clicked and update smiles as well as mw

--- client/src/components/AddReagentDialog.tsx
+++ client/src/components/AddReagentDialog.tsx
@@ -1,54 +1,117 @@
-import { DialogContent, DialogTitle, TextField } from "@mui/material"
+import { Button, DialogContent, DialogTitle, Stack, TextField, ToggleButton, ToggleButtonGroup } from "@mui/material"
 import { useEffect, useState } from "react"
 import MoleculeStructure from "./MoleculeStructure/MoleculeStructure"
 
 
-export const AddReagentDialog = () => {
-    const [eq, setEq] = useState<number>()
-    const [molecularWeight, setMolecularWeight] = useState<number>()
-    const [smilesInput, setSMILESInput] = useState<string>('')
+interface MoleculeInputProps {
+    moleculeInputType: string
+}
+
+// process the input and output a canonicalSMILES and molecular weight to the parent component
+// accepts either SMILES or the name of the reagent
+const MoleculeInputForm = ({ moleculeInputType }: MoleculeInputProps) => {
+    const [moleculeInput, setMoleculeInput] = useState<string>('')
+    // const [moleculeInputName, setMoleculeInputName] = useState<string>('')
+    const [helperText, setHelperText] = useState<string>()
+
+    // want to pass these things up to parent
     const [canonicalSMILES, setCanonicalSMILES] = useState<string>()
-    const [helperTextSMILES, setHelperTextSMILES] = useState<string>()
+    const [molecularWeight, setMolecularWeight] = useState<number>()
+
 
     useEffect(() => {
         // recalculate things whenever input is changed
         // if there is no input
-        if (smilesInput === '') {
-            setHelperTextSMILES('')
+        if (moleculeInput === '') {
+            setHelperText('')
             setCanonicalSMILES('')
         } else {
-            const mol = window.RDKit.get_mol(smilesInput)
-            if (mol?.is_valid) {
-                setCanonicalSMILES(mol.get_smiles())
-                const desc = mol?.get_descriptors()
-                if (desc) {
-                    const jsonDesc = JSON.parse(desc)
-                    setMolecularWeight(jsonDesc.exactmw)
-                    setHelperTextSMILES(`Calculated MW: ${molecularWeight}`)
+            if (moleculeInputType === 'SMILES') {
+                const mol = window.RDKit.get_mol(moleculeInput)
+                if (mol?.is_valid) {
+                    setCanonicalSMILES(mol.get_smiles())
+                    const desc = mol?.get_descriptors()
+                    if (desc) {
+                        const jsonDesc = JSON.parse(desc)
+                        setMolecularWeight(jsonDesc.exactmw)
+                        setHelperText(`Calculated MW: ${molecularWeight}`)
+                    }
+                } else {
+                    setHelperText('Invalid SMILES')
+                    setMolecularWeight(undefined)
                 }
             } else {
-                setHelperTextSMILES('Invalid SMILES')
-                setMolecularWeight(undefined)
+                console.log(molecularWeight)
+                setHelperText(`MW: ${molecularWeight}`)
             }
         }
-    }, [smilesInput, molecularWeight])
+    }, [moleculeInput, molecularWeight])
+
 
     return (
         <>
-            <DialogTitle>
-                Add reagent
-            </DialogTitle>
-            <DialogContent>
-                <TextField
-                    label="Molecule SMILES"
-                    autoFocus
-                    margin="normal"
-                    id="molecule-smiles"
-                    fullWidth
-                    variant="standard"
-                    helperText={helperTextSMILES}
-                    onChange={(event) => { setSMILESInput(event.target.value) }}
-                />
+            <Stack
+                direction='row'
+            >
+                {moleculeInputType === "SMILES" ?
+                    <TextField
+                        label="Molecule SMILES"
+                        autoFocus
+                        margin="normal"
+                        id="molecule-smiles"
+                        fullWidth
+                        variant="standard"
+                        helperText={helperText}
+                        onChange={(event) => { setMoleculeInput(event.target.value) }}
+                    />
+                    :
+                    <TextField
+                        label="Molecule name"
+                        autoFocus
+                        margin="normal"
+                        id="molecule-name"
+                        fullWidth
+                        variant="standard"
+                        helperText={helperText}
+                        onChange={async (event) => {
+                            setMoleculeInput(event.target.value)
+                        }}
+                    />
+                }
+
+                {/* add a button to execute a search on PubChem */}
+                {moleculeInputType === "Name"
+                    ? <Button
+                        onClick={async () => {
+                            type PubChemResponse = {
+                                "PropertyTable": {
+                                    "Properties": {
+                                        "CID": string,
+                                        "MolecularWeight": string,
+                                        "CanonicalSMILES": string
+                                    }[]
+                                }
+                            }
+                            const response = await fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${moleculeInput}/property/MolecularWeight,CanonicalSMILES/json`,)
+                            if (response.status === 404) {
+                                setHelperText('Not found in PubChem')
+                                setCanonicalSMILES('')
+                            }
+                            const result: PubChemResponse = await response.json()
+                            console.log("RESPONSE: ", result.PropertyTable)
+                            const pcProperties = result.PropertyTable.Properties[0]
+                            const pubchemSMILES = pcProperties.CanonicalSMILES
+                            console.log(pubchemSMILES)
+
+                            setMolecularWeight(Number(pcProperties.MolecularWeight))
+                            const mol = window.RDKit.get_mol(pubchemSMILES)
+                            const canonicalized = mol?.get_smiles()
+                            if (canonicalized) {
+                                setCanonicalSMILES(canonicalized)
+                            }
+                        }}
+                    >Search on PubChem</Button>
+                    : null}
                 {canonicalSMILES ?
                     <MoleculeStructure
                         id="molecule-structure"
@@ -59,20 +122,46 @@ export const AddReagentDialog = () => {
                     />
                     : null
                 }
-                <TextField
-                    label="Molecule name"
-                    autoFocus
-                    margin="normal"
-                    id="molecule-name"
-                    fullWidth
-                    variant="standard"
-                    onChange={() => {
-                        // TODO: add timeout so it doesn't fire incomplete requests
-                        // request from pubchem
-                        // get mw 
+            </Stack>
+        </>
+    )
+}
 
-                    }}
-                />
+
+export const AddReagentDialog = () => {
+    const [eq, setEq] = useState<number>()
+
+    const [moleculeInputType, setMoleculeInputType] = useState<string>('SMILES')
+
+
+
+
+    const handleMoleculeInputToggle = (_: React.MouseEvent<HTMLElement>, newInput: string | null) => {
+        if (newInput !== null) {
+            setMoleculeInputType(newInput)
+        }
+    }
+
+    return (
+        <>
+            <DialogTitle>
+                Add reagent
+            </DialogTitle>
+            <DialogContent>
+                <ToggleButtonGroup
+                    value={moleculeInputType}
+                    exclusive
+                    onChange={handleMoleculeInputToggle}
+                    aria-label="input type for molecule"
+                >
+                    <ToggleButton value="SMILES" aria-label="SMILES">
+                        SMILES
+                    </ToggleButton>
+                    <ToggleButton value="Name" aria-label="Name">
+                        Name
+                    </ToggleButton>
+                </ToggleButtonGroup>
+                <MoleculeInputForm moleculeInputType={moleculeInputType} />
                 <TextField
                     label="Equivalents"
                     autoFocus
